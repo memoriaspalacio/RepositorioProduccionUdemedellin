5ee8629456c0667773e34c943ff6c673ad10da9a
Jacob <jacc20223@gmail.com>
2026-09-08 01:44:33 -0500

    programacion contador enemigos

--- Assets/_Viejo Juego (Para minar de todo)/Scripts/ContadorEnemigos.cs
+++ Assets/_Viejo Juego (Para minar de todo)/Scripts/ContadorEnemigos.cs
@@ -8,17 +8,20 @@ public class ContadorEnemigos : MonoBehaviour
 {
 
     [SerializeField] private List<GameObject> _enemies = new List<GameObject>(); //lista para almacenar enemigos
+    private int enemyCount;
     [SerializeField] private TextMeshProUGUI totalEnemies;
-    
-    
+    [SerializeField] private TextMeshProUGUI defeatEnemies;
+
+
     //[SerializeField] private GameObject _victory;
     // Start is called before the first frame update
 
-    
+
     void Start()
     {
         _enemies.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));//al comenzar la escena busca todos los objetos con tag Enemy
         totalEnemies.text = _enemies.Count.ToString();
+        defeatEnemies.text = enemyCount.ToString();
         
     }
 
@@ -27,6 +30,8 @@ public class ContadorEnemigos : MonoBehaviour
     public void EnemyDied(GameObject enemy)
     {
         _enemies.Remove(enemy);
+        enemyCount++;
+        defeatEnemies.text = enemyCount.ToString();
         CheckForWinCondition();
     }
 
